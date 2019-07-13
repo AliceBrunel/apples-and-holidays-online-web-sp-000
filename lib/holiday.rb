bc8ea9b5f7492b05e74dd1a8c60e4785b472a749
Alice Brunel <alc.brunel@gmail.com>
2019-07-13 11:56:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each_pair do |season,holiday|
+    puts "#{season}:"
+      holiday.each_pair do |key,value|
+        puts "#{key}:#{value.join(", ")}"
+      end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
